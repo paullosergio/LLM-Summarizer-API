658289266c830d124f2f3c67c79f66fa81068260
paullosergio <paulomg1996@gmail.com>
2025-01-07 16:32:33 -0300

    Adiciona rota de verificação de status e melhora a formatação do código

--- node-api/src/app.ts
+++ node-api/src/app.ts
@@ -1,10 +1,14 @@
-import express, { Application } from 'express';
-import tasksRoutes from './routes/tasksRoutes';
+import express, { type Application } from "express";
+import tasksRoutes from "./routes/tasksRoutes";
 
 const app: Application = express();
 app.use(express.json());
 
 // Rotas
-app.use('/tasks', tasksRoutes);
+app.use("/tasks", tasksRoutes);
+
+app.get("/", (req, res) => {
+	return res.json({ message: "API is running." });
+});
 
 export default app;
--- node-api/src/index.ts
+++ node-api/src/index.ts
@@ -2,8 +2,8 @@ import dotenv from "dotenv";
 dotenv.config();
 import app from "./app";
 
-const PORT = process.env.PORT;
+const {PORT} = process.env;
 
 app.listen(PORT, () => {
-  console.log(`Node API rodando na porta ${PORT}`);
+  console.log(`Node API rodando na porta http://localhost:${PORT}`);
 });
